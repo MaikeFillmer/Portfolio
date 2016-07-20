90278a235f9f9969df87c6e0b9f3445ad079e2ac
Maike <Maike.Fillmer@gmail.com>
2016-07-20 19:46:51 -0400

    server

--- controller/routes.js
+++ controller/routes.js
@@ -7,7 +7,7 @@ var path = require('path');
 module.exports = function(app){
 
     app.get('/', function(req,res) {
-        res.sendFile(process.cwd() + "/views/index.html");
+        res.sendFile(process.cwd() + "/index.html");
 
     });
 
