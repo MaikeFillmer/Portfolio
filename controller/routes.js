c0c9acf6737f0157daa65a6e4edc8f684c0beea0
Maike <Maike.Fillmer@gmail.com>
2016-07-20 19:20:36 -0400

    server

---
+++ controller/routes.js
@@ -0,0 +1,15 @@
+// ================================================================================
+// ROUTER
+// ================================================================================
+
+var path = require('path');
+
+module.exports = function(app){
+
+    app.get('/', function(req,res) {
+        res.sendFile(process.cwd() + "/views/index.html");
+
+    });
+
+        
+}
---
+++ server.js
@@ -0,0 +1,51 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var methodOverride = require('method-override');
+//var mysql      = require('mysql');
+
+
+//connection
+//var connection = require('./config/connection.js');
+
+
+// EXPRESS CONFIGURATION
+var app = express(); // Tells node that we are creating an "express" server
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+
+// BodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+
+//handlebars
+//var exphbs = require('express-handlebars');
+// app.engine('handlebars', exphbs({
+//     defaultLayout: 'main',
+//     extname: '.handlebars',
+//     layoutsDir: './views/layouts'}));
+// app.set('view engine','handlebars');
+// app.set('views', __dirname + '/views');
+
+
+//method override
+app.use(methodOverride('_method'));
+
+//Start server
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
+
+//ROUTES
+
+
+require('./controllers/routes.js')(app); 
+//require('./controllers/html-routes.js')(app);
+
+
